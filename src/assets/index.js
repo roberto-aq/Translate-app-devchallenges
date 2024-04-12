db6c820b1edbc8f343ba2c9229a192a19d32a277
roberto-aq <baxro2000@gmail.com>
2024-04-11 21:28:02 -0500

    Actualizado el index.js assets

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,7 +3,7 @@ import BackgroundImage from './hero_img.jpg';
 import ExpandArrow from './Expand_down.svg';
 import ReactSvg from './react.svg';
 import HorizontalTop from './Horizontal_top_left_main.svg';
-import SortAlfa from './sort_alfa.svg';
+import SortAlfa from './Sort_alfa.svg';
 import SoundMaxFill from './sound_max_fill.svg';
 import Clipboard from './Copy.svg';
 
